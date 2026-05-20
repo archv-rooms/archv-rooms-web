057ffcff57a6a291664a3b2588af89448d29f066
Beyond-Isaac <isaacvcarv4lho@gmail.com>
2026-05-20 10:26:47 -0300

    fix: change localhost url to archv-rooms.onrender.com

--- src/app/core/services/game.service.ts
+++ src/app/core/services/game.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class GameService {
 
-  private baseUrl = 'http://localhost:3000';
+  private baseUrl = 'http://archv-rooms.onrender.com';
 
   constructor(private http: HttpClient) {}
 
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -44,7 +44,7 @@ interface AvatarResponse {
 export class UserService {
 
   private http = inject(HttpClient);
-  private apiUrl = 'http://localhost:3000/user';
+  private apiUrl = 'http://archv-rooms.onrender.com/user';
 
   private getHeaders(): HttpHeaders {
     const token = localStorage.getItem('@ProjetoX:token');
