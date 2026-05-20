c7ffe410db6ab27d7306156f2a44d0d7c204a92d
cofeezz <dokke.donkey@gmail.com>
2026-05-20 11:23:17 -0300

    fix: links

--- src/app/core/services/game.service.ts
+++ src/app/core/services/game.service.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class GameService {
 
-  private baseUrl = 'https://archv-rooms-web.vercel.app';
+  private baseUrl = 'https://archv-rooms-web.vercel.app/login';
 
   constructor(private http: HttpClient) {}
 
