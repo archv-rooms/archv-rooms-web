3e46be2f079a3efbbd0e8d7027b3a4473990d73c
cofeezz <dokke.donkey@gmail.com>
2026-05-21 10:07:18 -0300

    fixing links pt2

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
