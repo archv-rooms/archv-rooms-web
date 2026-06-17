74cc6c3091e57f73f05cdfdf80c00ff559ad1f8f
andreemartinns <andreemartinns2@gmail.com>
2026-06-17 12:41:01 -0300

    fix: corrigindo session

--- src/app/core/services/session.service.ts
+++ src/app/core/services/session.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { environment } from '../../../environments/environments'
 
 @Injectable({ providedIn: 'root' })
@@ -8,15 +8,20 @@ export class SessionService {
 
   constructor(private http: HttpClient) {}
 
+  private get headers(): HttpHeaders {
+    const token = localStorage.getItem('@archv:token')
+    return new HttpHeaders({ Authorization: `Bearer ${token}` })
+  }
+
   startSession(gameId: number) {
-    return this.http.post(`${this.api}/sessions/start`, { gameId })
+    return this.http.post(`${this.api}/sessions/start`, { gameId }, { headers: this.headers })
   }
 
   endSession(sessionId: number) {
-    return this.http.patch(`${this.api}/sessions/${sessionId}/end`, {})
+    return this.http.patch(`${this.api}/sessions/${sessionId}/end`, {}, { headers: this.headers })
   }
 
   getHistory() {
-    return this.http.get(`${this.api}/sessions/history`)
+    return this.http.get(`${this.api}/sessions/history`, { headers: this.headers })
   }
 }
