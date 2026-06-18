a08f3fc7135adea7eb9c58b0b44393e428e50581
andreemartinns <andreemartinns2@gmail.com>
2026-06-18 09:53:39 -0300

    adiciona login com google na landing page

--- src/app/features/landing/landing.component.ts
+++ src/app/features/landing/landing.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-landing',
@@ -9,4 +10,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent {}
+export class LandingComponent {
+  private authService = inject(AuthService);
+
+  loginWithGoogle(): void {
+    this.authService.loginWithGoogle();
+  }
+}
