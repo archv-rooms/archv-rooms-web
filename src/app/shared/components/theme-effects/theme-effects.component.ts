7a58d6e8fb2311565637de980c1b74fbaf8b32e7
andreemartinns <andreemartinns2@gmail.com>
2026-06-17 21:56:06 -0300

    feat: edit

--- src/app/shared/components/theme-effects/theme-effects.component.ts
+++ src/app/shared/components/theme-effects/theme-effects.component.ts
@@ -114,7 +114,7 @@ export class ThemeEffectsComponent implements AfterViewInit, OnDestroy {
     this.showBanner = true;
     this.cdr.detectChanges();
 
-    setTimeout(() => { this.bannerVisible = true; }, 800);
+    setTimeout(() => { this.bannerVisible = true; }, 100);
     setTimeout(() => { this.bannerHiding = true; this.bannerVisible = false; this.cdr.detectChanges(); }, 10000);
     setTimeout(() => { this.showBanner = false; this.bannerHiding = false; this.cdr.detectChanges(); }, 11200);
     setTimeout(() => { this.triggerBanner(); }, 30000);
